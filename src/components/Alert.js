0acf9f2d559034d36fd40bf3919c3d696b96e61e
Andrey Ageev <ageev@MacBook-Pro-Andrey.local>
2021-02-17 14:31:39 +0300

    alert-context DONE

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,16 @@
-export const Alert = ({alert})=> {
+import React, {useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
-  if(!alert) return null
+export const Alert = ()=> {
+  const {alert, hide} = useContext(AlertContext)
+
+  if(!alert.visible) return null
 
   return (
     <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
       <strong>Holy guacamole!</strong> 
       <span>{alert.text}</span>
-      <button type="button" className="close" aria-label="Close">
+      <button onClick={hide} type="button" className="close" aria-label="Close">
         <span aria-hidden="true">&times;</span>
       </button>
     </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,29 @@
+import React, {useState, useContext} from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
 export const Form = () => {
+  const [value, setValue] = useState('')
+  const alert = useContext(AlertContext)
+
+  const submitHandler = e => {
+    e.preventDefault()
+    if(value.trim()) {
+      alert.show('The note was created', 'success')
+      setValue('')
+    } else {
+      alert.show('Write your note')
+    }
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input 
           type="text" 
           className="form-control"
           placeholder="Your new note"
+          value={value}
+          onChange={e=>setValue(e.target.value)}
         />
       </div>
     </form>
